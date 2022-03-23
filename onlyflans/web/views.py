bcca3bd5cb5af06802031d7d7971f50f7394af90
Allan Morales Prado <allan.moralesp@utem.cl>
2022-03-23 19:20:31 -0300

    nuevo descubrimiento, GET crea nuevas rutas que no existen

--- onlyflans/web/views.py
+++ onlyflans/web/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Flan, ContactForm
 from .forms import ContactFormModelForm
