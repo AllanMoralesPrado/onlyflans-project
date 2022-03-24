6119a7fac472f0df586e1ec18914cdb8480624c1
Allan Morales Prado <allan.moralesp@utem.cl>
2022-03-24 14:31:25 -0300

    new Flan model fields - part 2

--- onlyflans/web/models.py
+++ onlyflans/web/models.py
@@ -8,9 +8,10 @@ class Flan(models.Model):
     image_url = models.URLField()
     slug = models.SlugField()
     is_private = models.BooleanField()
-    no_eggs = models.BooleanField()
-    no_sugar = models.BooleanField()
-    no_lactose = models.BooleanField()
+    no_eggs = models.BooleanField(default=False)
+    no_sugar = models.BooleanField(default=False)
+    no_lactose = models.BooleanField(default=False)
+    price = models.PositiveIntegerField(default=0)
 
 class ContactForm(models.Model):
     contact_form_uuid = models.UUIDField(default=uuid.uuid4,editable=False)
