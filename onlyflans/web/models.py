1aa1d210e2570e48013ef96c120753678a3ac6a6
Allan Morales Prado <allan.moralesp@utem.cl>
2022-03-28 02:20:40 -0300

    borrar db

--- onlyflans/web/models.py
+++ onlyflans/web/models.py
@@ -1,12 +1,14 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Flan(models.Model):
     flan_uuid = models.UUIDField()
     name = models.CharField(max_length=64)
     description = models.TextField()
     image_url = models.URLField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     is_private = models.BooleanField()
     no_eggs = models.BooleanField(default=False)
     no_sugar = models.BooleanField(default=False)
@@ -19,6 +21,52 @@ class ContactForm(models.Model):
     customer_name = models.CharField(max_length=64)
     message = models.TextField()
 
+class Category(models.Model):
+    name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('draft','Draft'),
+        ('published','Published'),
+    )            
     
-# Create your models here.
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=255)
+    image_url = models.URLField(default='http://pa1.narvii.com/6962/773484dfab00ca9fecc375a045ab58badc4582f2r1-360-360_00.gif')
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, unique_for_date='published', null=False, unique=True)
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    class Meta:
+        ordering = ('-published',)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    content = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("publish",)
+
+        def __str__(self) -> str:
+            return f"Comment by {self.name}"
--- onlyflans/web/views.py
+++ onlyflans/web/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
-from .models import Flan, ContactForm
+from .models import Flan, ContactForm, Post
 from .forms import ContactFormModelForm
 from django.contrib.auth.decorators import login_required
 
@@ -59,3 +61,22 @@ def contacto(request):
 
     return render(request, 'contactus.html', {'form':form})
 
+class BlogHomePageView(TemplateView):
+    template_name = "blog/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.postobjects.all()
+        return context
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post-detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = Post.objects.filter(slug=self.kwargs.get('slug'))
+        return context
+    
+    
